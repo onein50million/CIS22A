b3d9bee12e91d2109db08c314f73b5d05b6a49c0
onein50million <dman.wart@gmail.com>
2017-04-25 17:24:02 -0700

    Started CIS 22B

---
+++ CIS22A_Lab01B_WartDaniel/CIS22A_Lab01B_WartDaniel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main(){
+
+	int columnSpacing = 10;
+	std::string table[5][8] = {
+		{ "1", "New York", "NY", "8,491,079", "8,175,133", "+3.86%", "302.6 sq mi", "27,012 per sq mi" },
+		{ "2", "Los Angeles", "CA", "3,928,864", "3,792,621", "+3.59%", "468.7 sq mi", "8,092 per sq mi" },
+		{ "3", "Chicago", "IL", "2,722,389", "2,695,598", "+0.99%", "227.6 sq mi", "11,842 per sq mi" },
+		{ "4", "Houston", "TX", "2,239,558", "2,100,263", "+6.63%", "599.6 sq mi", "3,501 per sq mi" },
+		{ "5", "Philadelphia", "PA", "1,560,297", "1,526,006", "+2.25%", "134.1 sq mi", "11,379 per sq mi" }
+	};
+
+}
